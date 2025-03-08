626ea1c1cc731bc3675847826389462b88ba65fd
Sergio Endrigo <Endrigo@Endrigos-Macbook.local>
2025-03-08 11:45:12 -0300

    add classe de erro

--- src/main/java/com/estudos/meuprojetodetesteweb/exceptions/CamposObrigatoriosNaoPreenchidosException.java
+++ src/main/java/com/estudos/meuprojetodetesteweb/exceptions/CamposObrigatoriosNaoPreenchidosException.java
@@ -1,2 +1,8 @@
-package com.estudos.meuprojetodetesteweb.exceptions;public class CamposObrigatoriosNaoPreenchidosException {
+package com.estudos.meuprojetodetesteweb.exceptions;
+
+public class CamposObrigatoriosNaoPreenchidosException extends RuntimeException{
+
+    public CamposObrigatoriosNaoPreenchidosException(String mensagem) {
+        super(mensagem);
+    }
 }
